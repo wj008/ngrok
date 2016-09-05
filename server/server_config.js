bf112d3fa271462cec2725a8055495dcdd8c18f6
sdopx <26029682@qq.com>
2016-09-05 11:33:32 +0800

    Create server_config.js

---
+++ server/server_config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    //服务器HTTP端口
+    "httpPort": 88,
+    //HTTP请求超时时间(秒)
+    "httpreqTimeout": 60,
+    //用于和客户端通讯端口
+    "serverPort": 5200,
+    //用于开放申请代理的端口范围
+    "openPortRange": [1000, 20000],
+}
